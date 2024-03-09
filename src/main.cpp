fa33b795e501b82eac9865e0ea9c513aec6146cb
roman <roman.safron@aon.at>
2024-03-09 21:33:52 +0100

    feat: add some songs

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,10 @@ MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 const char *songs_1_MP3[] = {"dontgo.mp3", "pinguin.mp3", "dumbbell.mp3", "mrbluesky.mp3", "crabrave.mp3",
 							 "good4you.mp3", "bohemian.mp3", "moorhexe.mp3", "hedwig.mp3", "moveitmoveit.mp3",
-							 "faint.mp3", "wannabela.mp3", "omnissiah.mp3", "vampire.mp3", NULL};
+							 "faint.mp3", "wannabela.mp3", "omnissiah.mp3", "vampire.mp3", "paintitblack.mp3",
+							 "surf.mp3", "sympathyforthedevil.mp3", "turntostone.mp3", "amy.mp3", "dontstopmenow.mp3",
+							 "everybodyhides.mp3", "help.mp3", "honky.mp3", "hound.mp3", "hound.mp3", "igetaround.mp3",
+							 "isawher.mp3", "jailhouse.mp3", "killerqueen.mp3", "bottom2.mp3", NULL};
 int iteration = 0;
 bool notIncremented = true;
 bool isFirstIteration = true;
